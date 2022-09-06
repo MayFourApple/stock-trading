ef9d73ee5a7b330c54796b08ddb9c800ccf671a5
May Four Apple Espinosa <mayfourapple@gmail.com>
2022-09-06 15:16:15 +0800

    Add Devise for Admins and Traders

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,17 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+    :address              => ENV.fetch("SMTP_ADDRESS"),
+    :port                 => ENV.fetch("SMTP_PORT"),
+    :user_name            => ENV.fetch("SMTP_USER"),
+    :password             => ENV.fetch("SMTP_PASSWORD"),
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :traders
+  devise_for :admins
+
+  get 'admin', to: 'admin#index'
+  get 'trader', to: 'trader#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
