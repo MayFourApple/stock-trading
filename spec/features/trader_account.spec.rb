7c43f2394ffbfe4299d2728251adddae25694505
May Four Apple Espinosa <mayfourapple@gmail.com>
2022-09-16 19:52:41 +0800

    Add test for login

--- spec/features/trader_account.spec.rb
+++ spec/features/trader_account.spec.rb
@@ -11,4 +11,18 @@ RSpec.feature 'Trader Account', type: :feature do
     trader = Trader.first
     expect(trader).to have_attributes(email: 'example@email.com')
   end
+
+  scenario 'trader can login to their account' do
+    trader = Trader.create(email: 'example@email.com', password: 'password')
+    trader.confirm
+    trader.approve
+
+    visit '/traders/sign_in'
+    fill_in 'trader[email]', with: 'example@email.com'
+    fill_in 'trader[password]', with: 'password'
+    click_on 'Log in'
+
+    trader = Trader.first
+    expect(trader).to have_attributes(email: 'example@email.com')
+  end
 end
